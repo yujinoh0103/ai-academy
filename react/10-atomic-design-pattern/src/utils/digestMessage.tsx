650eb23f9db321b3d88e75f39dd713a0c290d878
yujinoh0103 <yujin0103@sookmyung.ac.kr>
2025-03-04 11:25:12 +0900

    0304 fetch 완료

---
+++ react/10-atomic-design-pattern/src/utils/digestMessage.tsx
@@ -0,0 +1,6 @@
+export async function digestMessage(message: string) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return hash;
+}
